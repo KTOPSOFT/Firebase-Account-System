2f67e0fec43f05ae8e38fa29e03df22157b01abb
Yuki Ono <108729931+KTOPSOFT@users.noreply.github.com>
2025-11-03 20:22:51 +0900

    Update GameManager.cs

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//             Code Check              //
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
